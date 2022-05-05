b3fe1b4ecdcd4f42982fae090a37c5d10d58db89
Christoffer Hammarström <christoffer.hammarstrom@linuxgods.com>
2022-05-05 16:10:08 +0200

    Simpler icon loading

--- src/main/java/com/linuxgods/kreiger/idea/pentaho/kettle/ImageUtil.java
+++ src/main/java/com/linuxgods/kreiger/idea/pentaho/kettle/ImageUtil.java
@@ -1,54 +1,68 @@
 package com.linuxgods.kreiger.idea.pentaho.kettle;
 
-import com.intellij.ui.JBColor;
 import com.intellij.ui.scale.ScaleContext;
-import com.intellij.util.ImageLoader;
+import com.intellij.util.SVGLoader;
+import com.intellij.util.ui.JBImageIcon;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.net.URL;
 
 import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 public class ImageUtil {
     public static final Color SPOON_STEP_BORDER_COLOR = new Color(0x3a, 0x64, 0x81);
-    public static final Image MISSING_ENTRY_IMAGE = loadMissingEntry();
-    public static final ImageIcon MISSING_ENTRY_ICON = new ImageIcon(MISSING_ENTRY_IMAGE);
+    public static final ImageIcon MISSING_ENTRY_ICON = loadMissingEntry();
 
-    @NotNull public static Image drawOnBackground(Color color, float alpha, Image image) {
-        //if (true) return image;
-        image = com.intellij.util.ui.ImageUtil.resize(image, 32, ScaleContext.create());
+    @NotNull
+    public static Image drawOnBackground(Color color, Image image) {
         BufferedImage background = com.intellij.util.ui.ImageUtil.createImage(34, 34, TYPE_INT_RGB);
-        Graphics2D g2 = background.createGraphics();
-        g2.setColor(color);
-        Composite composite = g2.getComposite();
-        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-        g2.fillRect(0, 0, background.getWidth(), background.getHeight());
-        g2.setColor(SPOON_STEP_BORDER_COLOR);
-        g2.drawRoundRect(0, 0, background.getWidth() - 1, background.getHeight() - 1, 5, 5);
-        g2.setComposite(composite);
-        g2.drawImage(image, 1, 1, 32, 32, null);
-        g2.dispose();
-        background = com.intellij.util.ui.ImageUtil.createRoundedImage(background, 7);
+        Graphics2D g = background.createGraphics();
+        g.setColor(color);
+        g.fillRect(0, 0, background.getWidth(), background.getHeight());
+        g.drawImage(image, 1, 1, 32, 32, null);
         return background;
     }
 
-    public static Image loadMissingEntry() {
-        Image image = ImageLoader.loadFromUrl(ImageUtil.class.getResource("/ui/images/missing_entry.svg"));
-        return drawOnBackground(Color.RED, 0.2f, image);
+    public static ImageIcon loadMissingEntry() {
+        Image image;
+        try {
+            image = loadSVG(ImageUtil.class.getResource("/ui/images/missing_entry.svg"), 32, 32);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new JBImageIcon(drawOnBackground(Color.RED, image));
     }
 
-    @NotNull public static Image graphImage(Image image) {
-        return drawOnBackground(Color.WHITE, 1, image);
+    @NotNull
+    public static Image graphImage(Image image) {
+        return drawOnBackground(Color.WHITE, image);
     }
 
-    @NotNull public static ImageIcon graphIcon(Image image) {
-        return new ImageIcon(graphImage(image));
+    @NotNull
+    public static ImageIcon graphIcon(Image image) {
+        return new JBImageIcon(graphImage(image));
     }
 
-    @NotNull public static ImageIcon graphIcon(URL resource) {
-        return graphIcon(ImageLoader.loadFromUrl(resource));
+    public static Icon graphIcon(URL url) {
+        try {
+            return graphIcon(loadSVG(url, 32, 32));
+        } catch (IOException e) {
+            return MISSING_ENTRY_ICON;
+        }
     }
+
+    @Nullable
+    private static BufferedImage loadSVG(URL url, int width, int height) throws IOException {
+        BufferedImage image;
+        image = (BufferedImage) SVGLoader.load(url, url.openStream(), ScaleContext.create(), width, height);
+        image = (BufferedImage) com.intellij.util.ui.ImageUtil.ensureHiDPI(image, ScaleContext.create());
+        return image;
+    }
+
+
 }
